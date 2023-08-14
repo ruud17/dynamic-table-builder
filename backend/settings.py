7038c0524ebb3060ec5c6e113c4e4c1726179de1
Mehmed Jusic <mehmedjusic@gmail.com>
2023-08-14 22:55:05 +0200

    Added missing manage.py file

--- backend/settings.py
+++ backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lf-h1zxd0-6fg4bf#hhw2bj1fo0a1*3vs4_xt%a0^(+$-&it@n'
+SECRET_KEY = 'django-insecure-82&928qrx15l(_n$!1to5y^$mtlv0(ooq_^fbo1^vxert+6ed*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
